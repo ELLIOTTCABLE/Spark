185f93df1e96a5d3eddeff936a5ff46c1c51e464
elliottcable <git@elliottcable.name>
2009-09-05 22:57:09 -0400

    Making Spark::Rake::SpeckTask a bit more flexible. Will now require file globs as you desire. (Hat tip to lsegal/YARD for that!)

--- lib/spark/rake/speck_task.rb
+++ lib/spark/rake/speck_task.rb
@@ -1,19 +1,34 @@
+require 'rake'
+require 'rake/tasklib'
+
 module Spark
   module Rake
-    class SpeckTask
+    
+    class SpeckTask < ::Rake::TaskLib
+      attr_accessor :name
+      attr_accessor :desc
+      attr_accessor :files
       
-      ##
-      # Called from a Rake execution environment, will define a new Rake task
-      # that executes all of your unbound Specks, recursively (thus running
-      # all defined specks).
-      def initialize
-        desc 'Recursively runs all unbound Specks'
-        task :run do
-          Speck::unbound.each {|speck| Spark.playback speck }
-        end
+      def initialize name = :specks
+        @name = name
+        @files = []
+        @desc = "Recursively runs all unbound Specks"
         
+        yield self if block_given?
+        self.files += ENV['SPECK_FILES'].split(/[ ,]/) if ENV['SPECK_FILES']
+        
+        define
       end
       
+      def define
+        # XXX: Why the hell is this necessary? God, I hate Rake.
+        Object.send :desc, @desc
+        Object.send :task, name do
+          [@files].flatten.map {|p| p.include?("*") ? Dir[p] : p }.flatten
+            .each {|f| require File.expand_path(f) }
+          Speck::unbound.each {|speck| Spark.playback speck }
+        end
+      end
     end
   end
 end
