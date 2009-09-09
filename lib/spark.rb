a5522fbe6c69c66d03c918920a462c9e571fe42a
elliottcable <git@elliottcable.name>
2009-09-09 13:28:18 -0400

    Supporting the new `Speck::Battery` (elliottcable/Speck@52991cf)

--- lib/spark.rb
+++ lib/spark.rb
@@ -6,63 +6,69 @@ module Spark
   Version = 0
   
   ##
-  # “Plays” a `Speck` back, recursively. This consists of:
+  # “Plays” a `Speck`, or `Speck:Battery`, recursively. This consists of:
   # 
-  # - Printing data about the `Speck`
-  # - Executing the `Speck`
-  # - Executing each `Check` belonging to the `Speck`
+  # - Printing data about the `Speck` or `Battery`
+  # - Executing the `Battery`
+  # - Executing the `Speck`(s)
+  # - Executing each `Check` belonging to the `Speck`(s)
   # - Printing data about each `Check` and its result
-  # - Recursively repeating the above for each child `Speck`
-  def self.playback speck, indent = 0
-    # TODO: FUCK FUCK FUCK THIS IS UGLY CODE ARRRGH
-    puts ("  " * indent) + speck.target.inspect if speck.target
-    indent += 1
-    
-    speck.execute
-    
-    checks = speck.checks.group_by do |check|
-      begin
-        check.execute unless check.status
-      rescue Speck::Exception::CheckFailed
-      end
-      puts ("  " * indent) + case check.status
-      when :passed then (" # " + check.status.to_s).green
-      when :failed then (" # " + check.status.to_s).red
-      when :future then (" # " + check.status.to_s).yellow
-      else              (" # " + check.status.to_s).cyan
-      end
-      check.status
-    end
-    
-    child_checks = speck.children
-      .inject({:passed => [], :failed => []}) do |children_checks, speck|
-        child_checks = Spark.playback speck, indent
-        child_checks.each do |k,v|
-          children_checks[k] ||= Array.new
-          children_checks[k] += v || Array.new
+  # - Recursively repeating the above for each child `Speck` or `Battery`
+  def self.playback target, indent = 0
+    if target.respond_to? :specks and target.respond_to? :targets
+      target.specks.each {|speck| Spark::playback speck }
+      target.targets.each {|object, battery| Spark::playback battery }
+    else
+      # TODO: FUCK FUCK FUCK THIS IS UGLY CODE ARRRGH
+      puts ("  " * indent) + target.target.inspect if target.target
+      indent += 1
+      
+      target.execute
+      
+      checks = target.checks.group_by do |check|
+        begin
+          check.execute unless check.status
+        rescue Speck::Exception::CheckFailed
         end
-        children_checks
+        puts ("  " * indent) + case check.status
+        when :passed then (" # " + check.status.to_s).green
+        when :failed then (" # " + check.status.to_s).red
+        when :future then (" # " + check.status.to_s).yellow
+        else              (" # " + check.status.to_s).cyan
+        end
+        check.status
       end
-    
-    child_checks.each do |k,v|
-      checks[k] ||= Array.new
-      checks[k] += v || Array.new
+      
+      child_checks = target.children
+        .inject({:passed => [], :failed => []}) do |children_checks, speck|
+          child_checks = Spark::playback speck, indent
+          child_checks.each do |k,v|
+            children_checks[k] ||= Array.new
+            children_checks[k] += v || Array.new
+          end
+          children_checks
+        end
+      
+      child_checks.each do |k,v|
+        checks[k] ||= Array.new
+        checks[k] += v || Array.new
+      end
+      
+      
+      indent -= 1
+      
+      # TODO: FUCK FUCK FUCK THIS IS EVEN UGLIER THAN THE ABOVE CODE!!!!1!1
+      total = checks.inject(0) {|t, (k,v)| t + v.size }
+      passed = checks[:passed].size
+      failed = checks[:failed].size
+      other = checks.inject(0) {|t, (k,v)| t += v.size unless [:passed, :failed].include? k; t }
+      puts ("  " * indent) + "(#{
+        checks[:failed].size > 0 ?
+          checks[:passed].size.to_s.red : checks[:passed].size.to_s.green
+        }#{other &&! other.zero? ? '/' + other.to_s.cyan : nil } of #{total})" unless total.zero?
+      
+      return checks
     end
-    
-    
-    indent -= 1
-    
-    # TODO: FUCK FUCK FUCK THIS IS EVEN UGLIER THAN THE ABOVE CODE!!!!1!1
-    total = checks.inject(0) {|t, (k,v)| t + v.size }
-    passed = checks[:passed].size
-    failed = checks[:failed].size
-    other = checks.inject(0) {|t, (k,v)| t += v.size unless [:passed, :failed].include? k; t }
-    puts ("  " * indent) + "(#{
-      checks[:failed].size > 0 ?
-        checks[:passed].size.to_s.red : checks[:passed].size.to_s.green
-      }#{other &&! other.zero? ? '/' + other.to_s.cyan : nil } of #{total})" unless total.zero?
-    
-    return checks
   end
   
 end
--- lib/spark/rake/speck_task.rb
+++ lib/spark/rake/speck_task.rb
@@ -10,9 +10,11 @@ module Spark
       attr_accessor :name
       attr_accessor :desc
       attr_accessor :files
+      attr_accessor :batteries
       
-      def initialize name = :specks
+      def initialize name = :specks, batteries = Array.new
         @name = name
+        @batteries = batteries.is_a?(Array) ? batteries : [batteries]
         @files = []
         @desc = "Recursively runs all unbound Specks"
         
@@ -28,15 +30,19 @@ module Spark
         # XXX: Why the hell is this necessary? God, I hate Rake.
         Object.send :desc, @desc
         Object.send :task, name do
+          # TODO: AND SUDDENLY, UGLY CODES! KILOBYTES OF THEM!
           [@files].flatten.map {|p| p.include?("*") ? Dir[p] : p }.flatten
             .each {|f| require File.expand_path(f) }
-          checks = Speck::unbound.inject(Array.new) do |acc, speck|
-            puts '-~- ' * 10 + '*' + ' -~-' * 10 if Speck::unbound.size > 1
-            acc << Spark.playback(speck)
-          end
-          puts '-~- ' * 10 + '*' + ' -~-' * 10 if Speck::unbound.size > 1
           
-          exit(1) if checks.map {|c| c[:failed].size }
+          targets = @batteries.empty? ? Speck::unbound : @batteries
+          checks = targets.inject(Array.new) do |acc, target|
+            puts '-~- ' * 10 + '*' + ' -~-' * 10 if targets.size > 1
+            target = target.call if target.respond_to? :call
+            acc << Spark.playback(target)
+          end
+          puts '-~- ' * 10 + '*' + ' -~-' * 10 if targets.size > 1
+          
+          exit(1) if checks.map {|c| c[:failed] ? c[:failed].size : 0 }
             .inject {|acc,e| acc + e } > 0
         end
       end
